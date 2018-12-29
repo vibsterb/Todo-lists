c73fde21050268f3a7ee8bf94e1c4922aadb8dcc
Vibeke Braaten <vibeke.braaten@gmail.com>
2018-12-29 21:27:18 +0100

    moved menu-button, adjusted input text

--- DatabaseScripts/Lists.sql
+++ DatabaseScripts/Lists.sql
@@ -4,6 +4,7 @@ owner int,
 name text NOT NULL,
 done BOOLEAN default false,
 shareduser int,
+icons BOOLEAN default true,
 FOREIGN KEY (owner) REFERENCES "Users"(id),
 FOREIGN KEY (shareduser) REFERENCES "Users"(id)
 )
